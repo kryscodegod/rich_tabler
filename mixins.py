d72a2e0aa8146b621f0d9f0cdb7c4181ff97def2
Kristina Tyan <krysuserbot@rambler.ru>
2025-12-10 16:12:28 +0300

    done

--- mixins.py
+++ mixins.py
@@ -6,10 +6,10 @@ class RuText:
     info: str = 'Класс для демонстрации примеров создания простых таблиц с помощью данного пакета'
     warn: str = '[red]Ошибка! Неверный формат данных!'
     note: str = '[red]Что-то пошло не так!'
-    try_again: str = '[green] Попробуйте ввести данные в формате: [bold yellow]list<tuple>'
-    set_data_first: str  = '[green] Введите список названий колонок в формате: [bold yellow]list<str>'
-    set_data_second: str = '[green] Затем введите данные для создания столбцов в формате: [bold yellow]list<dict<str, str>>'
-    set_data_alt: str = '[green] Вы также можете ввести данные в формате [bold yellow]list<tuple> [green]с методом ~ [bold yellow]create_from_standard_content()'
+    try_again: str = '[green] Попробуйте передать в конструктор данные в формате: [bold yellow]list<tuple>'
+    set_data_first: str  = '[green] Передайте в конструктор список названий колонок в формате: [bold yellow]list<str>'
+    set_data_second: str = '[green] Затем передайте в конструктор данные для создания столбцов в формате: [bold yellow]list<dict<str, str>>'
+    set_data_alt: str = '[green] Вы также можете передать в конструктор данные в формате [bold yellow]list<tuple> [green]с методом ~ [bold yellow]create_from_standard_content()'
     set_data_warn: str = '[red]Помните, что  rich.Table принимает только renderable-объекты и строки!'
 
     def __post_init__(self):
